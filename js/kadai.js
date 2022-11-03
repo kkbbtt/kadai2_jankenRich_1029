dd9adba874c636e0b60ea8d9a0827c48049fba26
kkbbtt <kbtkzhr@gmail.com>
2022-11-03 14:49:43 +0900

    不具合あり

--- js/kadai.js
+++ js/kadai.js
@@ -6,18 +6,23 @@
  * として処理する
  */
 
-
+const btn3 = document.getElementById('btn3');
+const mask = document.getElementById('mask');
+const modal = document.getElementById('modal');
 
 $(".img_janken0").on("click", function () {
     var random = Math.floor(Math.random() * 2);
     console.log(random, "ランダムな数字");
 
     if (random === 0) {
-        window.location.href = 'draw.html';
+        mask_draw.classList.remove('hidden');
+        modal_draw.classList.remove('hidden');
       } else if (random === 1) {
-        window.location.href = 'lose.html';
+        mask_lose.classList.remove('hidden');
+        modal_lose.classList.remove('hidden');
       } else if (random === 2) {
-        window.location.href = 'win.html';
+        mask_win.classList.remove('hidden');
+        modal_win.classList.remove('hidden');
       }
 
 });
@@ -52,22 +57,17 @@ $(".img_janken2").on("click", function () {
 
 });
 
+mask_win.addEventListener('click', () => {
+  mask_win.classList.add('hidden');
+  modal_win.classList.add('hidden');
+});
 
-window.onload = function () {
-    var popup = document.getElementById('js-popup');
-    if (!popup) return;
-    popup.classList.add('is-show');
-    
-    var blackBg = document.getElementById('js-black-bg');
-    var closeBtn = document.getElementById('js-close-btn');
-    
-    closePopUp(blackBg);
-    closePopUp(closeBtn);
-    
-    function closePopUp(elem) {
-    if (!elem) return;
-    elem.addEventListener('click', function () {
-    popup.classList.remove('is-show');
-    })
-    }
-    }
+mask_draw.addEventListener('click', () => {
+  mask_draw.classList.add('hidden');
+  modal_draw.classList.add('hidden');
+});
+
+mask_lose.addEventListener('click', () => {
+  mask_lose.classList.add('hidden');
+  modal_lose.classList.add('hidden');
+});
